b6ef373fff90d6846f6c00da1eec543fe854eb02
PrinceD08 <princedavid2001101@gmail.com>
2025-12-02 22:23:05 +0100

    Asjusted Style

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ function dropdown(){
     document.getElementById("dropdownmenu").style.opacity = "100";
     document.getElementById("heading").textContent = "";
 }
+function displayoutput1(){
+    document.getElementById("output1").textContent = "Luxury";
+}
+function displayoutput2(){
+    document.getElementById("output2").textContent = "Sport On Road";
+}
+function displayoutput3(){
+    document.getElementById("output3").textContent = "Classic";
+}
+function displayoutput4(){
+    document.getElementById("output4").textContent = "SUV in Nature";
+}
+function displayoutput5(){
+    document.getElementById("output5").textContent = "Luxury Car Interior";
+}
+function displayoutput6(){
+    document.getElementById("output6").textContent = "Classic Car Detail";
+}
